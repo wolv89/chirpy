d4e0b97f5ce4db861d22a18f8f7cb2919fbca53d
wolv89 <peter.delafosse@gmail.com>
2024-11-18 17:53:55 +1100

    Basic server setup

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	// ...
+
+	port := "8080"
+	filepathRoot := "."
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handleHealthCheck)
+
+	srv := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+
+}
+
+func handleHealthCheck(w http.ResponseWriter, _ *http.Request) {
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
 }
